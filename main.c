ba2af7d78b33bf4900e712cc0e35222c5b3e1042
Shanisanal <shani.sanal@trenser.com>
2026-03-25 12:03:53 +0530

    Updated main.c header summary

---
+++ main.c
@@ -0,0 +1,30 @@
+//**************************** Pthreads_Task **********************************
+//  Copyright (c) 2026 Trenser Technology Solutions
+//  All Rights Reserved 
+//*****************************************************************************
+//
+// File      : main.c
+// Summary   : This program implements Multithreading and Inter-Process Communication.
+// Note      : None
+// Author    : Shani
+// Date      : Mar 25, 2026
+//
+//***************************************************************************** 
+
+//******************************* Include Files *******************************
+
+//******************************* Local Types ********************************* 
+ 
+//***************************** Local Constants ******************************* 
+
+//***************************** Local Variables ******************************* 
+
+//****************************** Local Functions ****************************** 
+
+//*********************************** main *************************************
+//Purpose   : Entry point of the thread application.
+//Inputs    : None
+//Outputs   : None
+//Return    : 0 on successful completion, 1 if thread execution fails.
+//Notes     : None
+//******************************************************************************
