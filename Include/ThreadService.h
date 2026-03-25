8955cc90b0058d70f80e9994ab8cb8a289eaf0b2
Shanisanal <shani.sanal@trenser.com>
2026-03-25 14:38:53 +0530

    Header file for the function declarations in  ThreadService.c

---
+++ Include/ThreadService.h
@@ -0,0 +1,32 @@
+//************************** Thread_IPC ***************************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : ThreadService.h
+// Summary  : Header file defining the thread service functions.
+// Note     : None
+// Author   : Shani 
+// Date     : Mar 25, 2026 
+//
+//*****************************************************************************
+#ifndef THREADSERVICE_H
+#define THREADSERVICE_H
+
+//******************************* Include Files ******************************* 
+#include "Common.h"
+
+//******************************* Global Types ******************************** 
+ 
+//***************************** Global Constants ****************************** 
+
+//***************************** Global Variables ****************************** 
+
+//**************************** Forward Declarations *************************** 
+bool ThreadHandlerRun(void);
+
+//*********************** Inline Method Implementations *********************** 
+
+#endif  /*THREADSERVICE_H*/
+
+// EOF 
