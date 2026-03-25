bc656643e7141886df218778480678c2e8a5047c
Shanisanal <shani.sanal@trenser.com>
2026-03-25 12:15:54 +0530

    Corrected main.c header summary

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//**************************** Pthreads_Task **********************************
+//**************************** Thread_IPC *************************************
 //  Copyright (c) 2026 Trenser Technology Solutions
 //  All Rights Reserved 
 //*****************************************************************************
@@ -28,3 +28,18 @@
 //Return    : 0 on successful completion, 1 if thread execution fails.
 //Notes     : None
 //******************************************************************************
+int main()
+{
+    int lReturnCode = 0;
+/*     bool blStatus = true;
+
+    blStatus = ThreadHandlerRun();
+
+    if(false == blStatus)
+    {
+        printf("Critical Error: Thread execution failed.\n");
+        lReturnCode = 1; 
+    } */
+
+    return lReturnCode; 
+}
