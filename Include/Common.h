11f5aa3e91dc8b44d1c1ef2ac7fe9970e1d31e23
Shanisanal <shani.sanal@trenser.com>
2026-03-25 12:15:11 +0530

    Updated Common.h header summary

---
+++ Include/Common.h
@@ -0,0 +1,41 @@
+//************************** Thread_IPC ***************************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : Common.h
+// Summary  : Contains Centralized definitions, shared structures, and global 
+//            declarations
+// Note     : None
+// Author   : Shani 
+// Date     : Mar 25, 2026 
+//
+//*****************************************************************************
+#ifndef COMMON_H
+#define COMMON_H
+
+//******************************* Include Files ******************************* 
+#include <pthread.h>
+#include <mqueue.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+//******************************* Global Types ******************************** 
+ 
+//***************************** Global Constants ****************************** 
+
+//***************************** Global Variables ****************************** 
+
+//**************************** Forward Declarations *************************** 
+
+//*********************** Inline Method Implementations *********************** 
+
+#endif  /*COMMON_H */
+
+// EOF 
