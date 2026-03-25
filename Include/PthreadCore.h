a55b0d3fd49ac922bd27857d96b9f8b80cb1c4ae
Shanisanal <shani.sanal@trenser.com>
2026-03-25 15:35:06 +0530

    Function declaration header file for PthreadCore source file

---
+++ Include/PthreadCore.h
@@ -0,0 +1,34 @@
+//************************** Thread_IPC ***************************************
+// Copyright (c) 2026 Trenser Technology Solutions
+// All Rights Reserved
+//*****************************************************************************
+//
+// File     : PthreadCore.h
+// Summary  : Function declarations of wrapper functions in PthreadCore.c
+// Note     : None
+// Author   : Shani 
+// Date     : Mar 25, 2026 
+//
+//*****************************************************************************
+#ifndef PTHREADCORE_H
+#define PTHREADCORE_H
+
+//******************************* Include Files ******************************* 
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "Common.h"
+
+//******************************* Global Types ******************************** 
+
+//***************************** Global Constants ****************************** 
+
+//***************************** Global Variables ****************************** 
+
+//**************************** Forward Declarations *************************** 
+
+//*********************** Inline Method Implementations *********************** 
+
+#endif  /*PTHREADCORE_H */
+
+// EOF 
